9f39a09a9e2d56836f204babc3d9a95791cdf658
Banny Vishwas <bannyvishwas2012@gmail.com>
2022-02-04 15:58:03 +0530

    added book texture and animation

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,5 @@
+let hoverbook = true;
+let currentActive = "no-book";
 $(document).ready(function(){
     var screen_height = $(window).height();
     var screen_width = $(window).width();
@@ -13,5 +15,39 @@ $(document).ready(function(){
     });
     $("#room").click(function(){
         $("#room").css({"transform":"scale(1)"});
-    }); 
+    });
+    
+    $(".books").click(function(){
+        var getName = $(this).attr("alt");
+        if(currentActive === getName || currentActive === "no-book"){
+            if (hoverbook === true){
+                $(this).removeClass("hoverbook");
+                $(this).addClass("selectbook");
+                $(this).parent().addClass("setZIndex");
+                hoverbook = false;
+                currentActive = getName;
+                
+            }else{
+                // Add Hover and remove Select
+                $(this).removeClass("selectbook");
+                $(this).addClass("hoverbook");
+                hoverbook = true;
+                currentActive = "no-book";
+                $(this).parent().removeClass("setZIndex");
+            }
+        }else{
+            // Close Current Book, and Open new Book
+            var getBook = $('*[alt="'+currentActive+'"]');
+            getBook.removeClass("selectbook");
+            getBook.addClass("hoverbook");
+            getBook.parent().removeClass("setZIndex");
+
+            $(this).removeClass("hoverbook");
+            $(this).addClass("selectbook");
+            $(this).parent().addClass("setZIndex");
+
+            hoverbook = false;
+            currentActive = getName;
+        }
+    });
   });
