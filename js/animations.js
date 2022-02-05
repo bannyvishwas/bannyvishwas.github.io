8babe69d602e59e7974fdec02c1c3e72858e9319
Bholanath Vishwas <bholanathvishwas@globussoft.in>
2022-02-05 15:03:40 +0530

    fixed responsive issue

--- js/animations.js
+++ js/animations.js
@@ -1,22 +1,34 @@
 let hoverbook = true;
 let currentActive = "no-book";
+ // Change Scale to 1 before refreshing
+ $(window).bind('beforeunload',function(){
+    $("#room").css({"transform":"scale(1)","transform-origin":"0% 0% 0px"});
+});
 $(document).ready(function(){
-    var screen_height = $(window).height();
+   // Change Height and width of Body
+    /*var screen_height = $(window).height();
     var screen_width = $(window).width();
     $("body").css({"min-height":screen_height,"min-width":screen_width});
-    
+    */
+
+    // Zoom Desktop
     $("#room #desktopscr").click(function(e){
         $("#room").css({"transform":"scale(5)","transform-origin":"53% 41% 0px"});
         e.stopPropagation();
     });
+
+    //Zoom Bookrack
     $("#room #bookrack").click(function(e){
         $("#room").css({"transform":"scale(3)","transform-origin":"90% 10% 0px"});
         e.stopPropagation();
     });
+
+    // Zoom Out Room
     $("#room").click(function(){
         $("#room").css({"transform":"scale(1)"});
     });
     
+    // Books Animation
     $(".books").click(function(){
         var getName = $(this).attr("alt");
         if(currentActive === getName || currentActive === "no-book"){
